eb8a8fe634645e21ea3d142f68ffb02a6c81b509
Lorraine-alt <lozanolorraine812@gmail.com>
2024-11-04 18:26:32 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,9 @@
+// Collecting user information
+const subjectTitle = "Data Structure and Algorithm"; // Replace with actual input
+const classSchedule = "9:00 AM - 10:00 AM, TUE-THU"; // Replace with actual input
+const classroom = "comlab a"; // Replace with actual input
+const classInstructor = "Sir. Vj Varayuga"; // Replace with actual input
+const studentName = "Mary Lorraine D. Lozano"; // Replace with actual input
+
+// Logging the information to the console
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}.
